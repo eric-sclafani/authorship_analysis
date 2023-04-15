8b783a786b0222099a2fec5f28142e236283d29b
eric-sclafani <eric.sclafani321@gmail.com>
2023-04-15 19:57:38 -0400

    added lots of stuff

---
+++ app.py
@@ -0,0 +1,30 @@
+
+from dash import html, Dash, dcc
+import dash_bootstrap_components as dbc
+from components import plots
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
+app.title = "Authorship Analysis"
+
+
+app.layout = html.Div([
+    html.Div(className="main-div",
+             children=[
+                 html.H1("Authorship Analysis", className="h1"),
+                 html.Hr(),
+                 html.H4("By: Eric Sclafani", className="h4"),
+                 html.Hr()]),
+    
+    html.Div(className="tabs-div",
+             children=[
+                 dcc.Graph(figure=plots.document_vector_plot())])
+    ]
+                      )
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
