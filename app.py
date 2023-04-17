478ea3240a35100e73d6d75fef1c591435dc12a6
eric-sclafani <eric.sclafani321@gmail.com>
2023-04-17 19:15:25 -0400

    set up interactions between av and dv plots

--- app.py
+++ app.py
@@ -1,27 +1,49 @@
 
+import dash
+import pandas as pd
+from dash.dependencies import Input, Output
 from dash import html, Dash, dcc
 import dash_bootstrap_components as dbc
 from components import scatter_plots
+
+
+#~~~Data~~~
+docs_df = pd.read_csv("data/document_vectors.csv")
+authors_df = pd.read_csv("data/author_vectors.csv")
+
+#~~~App~~~
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.LITERA])
 app.title = "Authorship Analysis"
 
 app.layout = html.Div([
     
-    html.Div(className="tabs-div",
+    html.Div(className="main-div",
              children=[
                  dbc.Container([
-                     dbc.Row([dbc.Col(dcc.Graph(id="av-plot", figure=scatter_plots.author_vector_plot())),
-                              dbc.Col(dcc.Graph(id="dv-plot",figure=scatter_plots.document_vector_plot())),
-                              ])
-                     ])
+                     dbc.Row(
+                         [
+                             dbc.Col(dcc.Graph(id="av-plot", figure=scatter_plots.author_vector_plot())),
+                             dbc.Col(dcc.Graph(id="dv-plot"))
+                         ],
+                         className="g-0")])
                  ]
              )
     ])
 
 
+#~~~Callbacks~~~
 
+@dash.callback(Output("dv-plot", "figure"),
+               Input("av-plot", "hoverData"))
+def update_dv_plot_hover(selected_author):
 
+    if selected_author:
+        author_index = selected_author["points"][0]["pointIndex"]
+        return scatter_plots.document_vector_plot(author_index)
+    else:
+        return scatter_plots.document_vector_plot()
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
--- components/scatter_plots.py
+++ components/scatter_plots.py
@@ -13,61 +13,45 @@ warnings.simplefilter(action='ignore', category=DeprecationWarning)
 
 
 #~~~Data~~~
-docs_df = pd.read_csv("data/document_vectors.csv")
+
 authors_df = pd.read_csv("data/author_vectors.csv")
+author_pipeline = Pipeline([
+    ("scaler", StandardScaler()),
+    ("MDS", MDS())
+    ])
+author_vectors = authors_df.loc[:, ~authors_df.columns.isin(['author_id'])]
+processed_author_vectors = author_pipeline.fit_transform(author_vectors)
+author_kmeans = KMeans(n_clusters=6, random_state=42)
+author_kmeans.fit(processed_author_vectors) 
 
-# ~~~Pipelines~~~
 
+docs_df = pd.read_csv("data/document_vectors.csv")
 doc_pipeline = Pipeline([
     ("scaler", StandardScaler()),
     ("PCA", PCA(n_components=50)),
     ("TSNE", TSNE())
     ])
+doc_vectors = docs_df.loc[:, ~docs_df.columns.isin(['doc_id', 'author_id'])]
+processed_doc_vectors = doc_pipeline.fit_transform(doc_vectors)
+doc_kmeans = KMeans(n_clusters=6, random_state=42)
+doc_kmeans.fit(processed_doc_vectors)
 
-author_pipeline = Pipeline([
-    ("scaler", StandardScaler()),
-    ("MDS", MDS())
-    ])
 
-   
- 
+#~~~Helpers~~~
+
+def get_docs_given_author(author_index:int, df:pd.DataFrame) -> pd.DataFrame:
+    """Retrieves document records written by a given author"""
+    return df.loc[df['author_id'] == authors_df.iloc[author_index].author_id]
+
+
 #~~~ Plot functions ~~~   
-def document_vector_plot():
-    
-    docs_values = docs_df.loc[:, ~docs_df.columns.isin(['doc_id', 'author_id'])]
-    transformed_data = doc_pipeline.fit_transform(docs_values)
-    
-    kmeans = KMeans(n_clusters=6, random_state=42)
-    kmeans.fit(transformed_data)
-    df = pd.DataFrame({"author_id":docs_df["author_id"],
-                       "TSNE Dim 1":transformed_data[:,0],
-                       "TSNE Dim 2": transformed_data[:,1],
-                       "K Cluster":kmeans.labels_})
-    fig = px.scatter(
-        data_frame = df,
-        x="TSNE Dim 1",
-        y="TSNE Dim 2",
-        color="K Cluster",
-        color_continuous_scale="fall"
-    )
-    
-    fig.update_layout(
-        title="Document Vectors",
-        title_x=0.5)
-    return fig
-    
-    
+  
 def author_vector_plot():
 
-    authors_values = authors_df.loc[:, ~authors_df.columns.isin(['author_id'])]
-    transformed_data = author_pipeline.fit_transform(authors_values)
-    
-    kmeans = KMeans(n_clusters=6, random_state=42)
-    kmeans.fit(transformed_data)
     df = pd.DataFrame({"author_id":authors_df["author_id"],
-                       "MDS Dim 1":transformed_data[:,0],
-                       "MDS Dim 2": transformed_data[:,1],
-                       "K Cluster": kmeans.labels_})
+                       "MDS Dim 1":processed_author_vectors[:,0],
+                       "MDS Dim 2": processed_author_vectors[:,1],
+                       "K Cluster": author_kmeans.labels_})
     fig = px.scatter(
         data_frame = df,
         x="MDS Dim 1",
@@ -78,6 +62,34 @@ def author_vector_plot():
     
     fig.update_layout(
         title="Author Vectors",
-        title_x=0.5)
+        title_x=0.2,
+        autosize=False,
+        width=500,
+        height=350,
+        )
     return fig
 
+def document_vector_plot(selected_author=None):
+    
+    df = pd.DataFrame({"author_id":docs_df["author_id"],
+                       "TSNE Dim 1":processed_doc_vectors[:,0],
+                       "TSNE Dim 2": processed_doc_vectors[:,1],
+                       "K Cluster":doc_kmeans.labels_})
+    df_to_show = df if selected_author is None else get_docs_given_author(selected_author, df)
+    
+    fig = px.scatter(
+        data_frame = df_to_show,
+        x="TSNE Dim 1",
+        y="TSNE Dim 2",
+        color="K Cluster",
+        color_continuous_scale="fall"
+    )
+    fig.update_layout(
+        title="Document Vectors",
+        title_x=0.5,
+        autosize=False,
+        width=750,
+        height=350,
+        margin=dict(l=0, r=0))
+    
+    return fig
