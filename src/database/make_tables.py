d66e6f1fc409fca5ae7033e07b8040ba9d9bce1e
eric-sclafani <eric.sclafani321@gmail.com>
2023-06-10 21:36:20 -0400

    merged make_tables and create_author_vectors

--- src/database/make_tables.py
+++ src/database/make_tables.py
@@ -3,18 +3,34 @@
 This script is for making postgres tables for a dataset on the document and author levels
 """
 import pandas as pd
+import numpy as np
 import argparse
 from sqlalchemy import create_engine
+from dataclasses import dataclass
 
 from typing import Dict
 
 # project imports
-from connect import config, postgres_connection
-from create_author_vectors import make_author_vector_df
-
+from connect import config
 
 def load_document_vectors(path:str) -> pd.DataFrame:
     return pd.read_csv(path)
+
+def get_author_ids(doc_df:pd.DataFrame) -> np.ndarray:
+    return doc_df.authorIDs.unique()
+
+def make_author_vector(author_id:str, doc_df:pd.DataFrame) -> pd.Series:
+    author_document_vectors = doc_df.loc[doc_df['authorIDs'] == author_id]
+    return author_document_vectors.mean(axis=0, numeric_only=True)
+
+def make_author_vector_df(doc_df:pd.DataFrame) -> pd.DataFrame:
+    """Creates author vectors by averaging each author's documents into one"""
+    author_ids = get_author_ids(doc_df)
+    author_ids_to_avs = {}
+    for author_id in author_ids:
+        author_ids_to_avs[author_id] = make_author_vector(author_id, doc_df)    
+    av_df = pd.DataFrame(author_ids_to_avs).T
+    return av_df
 
 
 def main():
@@ -31,25 +47,17 @@ def main():
     
     args = parser.parse_args()
     
-    # document_vectors = load_document_vectors(args.document_vectors_path)
-    # author_vectors = make_author_vector_df(document_vectors)
-    
+    document_vectors = load_document_vectors(args.document_vectors_path)
+    author_vectors = make_author_vector_df(document_vectors)
     
     cfg = config()
     engine = create_engine(f'postgresql://{cfg["user"]}:{cfg["password"]}@{cfg["host"]}:{cfg["port"]}/{cfg["database"]}')
-    df = pd.DataFrame({
-        "col1":[1,2,3],
-        "col2":[4,5,6],
-        "col3":[7,8,9]
-    })
-    df.to_sql("test_table", engine, index=False, if_exists="replace")
     
+
     
-    with postgres_connection() as connection:
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM test_table;")
-        result = cursor.fetchall()
-        print(result)
+    document_vectors.to_sql(f"{args.name}_document_vectors", engine, index=False, if_exists="fail")
+    #author_vectors.to_sql(f"{args.name}_author_vectors", engine, index=False, if_exists="fail")
+
 
 if __name__ == "__main__":
     main()
