d24961aaf7a2fb7787e6cbcfd06fa2f27f6382b6
eric-sclafani <eric.sclafani321@gmail.com>
2023-06-25 13:18:50 -0400

    added bin

---
+++ src/bin/determine_k.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+This script is for determining the appropriate k value for Kmeans using the elbow method
+https://www.analyticsvidhya.com/blog/2021/01/in-depth-intuition-of-k-means-clustering-algorithm-in-machine-learning/
+"""
+
+
